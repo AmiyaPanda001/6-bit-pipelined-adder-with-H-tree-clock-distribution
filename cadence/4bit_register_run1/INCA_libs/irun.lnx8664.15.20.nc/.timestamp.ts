adb02a1bfe92eb81d21f961d94fc0d407d1e3fbd
Amiya Panda <amiyaranjanpanda001@gmail.com>
2019-12-01 17:28:54 -0600

    Library - ~/cadence/Design

---
+++ cadence/4bit_register_run1/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,5 @@
+1567910410 /home/grads/a/amiya_1/ECEN714/lab_01/cadence/4bit_register_run1/ihnl/cds0/netlist
+1139589194 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/Dlatch/behavioral/verilog.v
+1139589195 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/inv/functional/verilog.v
+1567910410 /home/grads/a/amiya_1/ECEN714/lab_01/cadence/4bit_register_run1/ihnl/cds1/netlist
+1567910410 /home/grads/a/amiya_1/ECEN714/lab_01/cadence/4bit_register_run1/testfixture.template
---
+++ cadence/fulladder_run1/INCA_libs/irun.lnx8664.15.20.nc/.timestamp.ts
@@ -0,0 +1,4 @@
+1567813696 /home/grads/a/amiya_1/ECEN714/lab_01/cadence/fulladder_run1/ihnl/cds0/netlist
+1139589200 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/xor2/functional/verilog.v
+1567813696 /home/grads/a/amiya_1/ECEN714/lab_01/cadence/fulladder_run1/testfixture.template
+1139589197 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/nand2/functional/verilog.v
